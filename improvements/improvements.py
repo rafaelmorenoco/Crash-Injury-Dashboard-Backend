ca8840815f50ab97e47e0236ba989fe023f9ebcb
Rafael Moreno <49096507+rafaelmorenoco@users.noreply.github.com>
2025-06-18 15:55:22 +0000

    EDA for ts_lpi

--- improvements/improvements.py
+++ improvements/improvements.py
@@ -12,13 +12,15 @@ from arcgis.features import FeatureLayer
 # Get credentials from environment variables
 client_id = os.environ.get('ARCGIS_CLIENT_ID')
 client_secret = os.environ.get('ARCGIS_CLIENT_SECRET')
-feature_layer_id = os.environ.get('ARCGIS_FEATURE_LAYER_ID')
+feature_layer_ts = os.environ.get('ARCGIS_FEATURE_LAYER_TrafficSignals_LPI')
 
 # Connect to ArcGIS
-gis = GIS("https://dcgis.maps.arcgis.com", client_id=client_id, client_secret=client_secret)
-feature_layer_item = gis.content.get('XXXXXXXXXXXXX')
-feature_layer = feature_layer_item.layers[0]  # Access the first layer in the item
+gis = GIS("https://dcgis.maps.arcgis.com",
+          client_id=client_id, client_secret=client_secret)
+feature_layer_item = gis.content.get(feature_layer_ts)
+# Access the first layer in the item
+feature_layer = feature_layer_item.layers[0]
 
 # Query all features
 features = feature_layer.query(where="1=1", out_fields="*")
-df = features.sdf
+ts_lpi = features.sdf
